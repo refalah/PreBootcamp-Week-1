dbbdf6d4ce71d5f97796ceb233a9dc7ad6b57b4a
Rezadi Falah <rezadifalah@gmail.com>
2021-02-24 13:45:57 +0700

    test

--- 2_Reverse/reverse.js
+++ 2_Reverse/reverse.js
@@ -1,15 +1,15 @@
 
 
-function revNum(num){
+function revNum(num) {
     x = num.toString();
     return x.split("").reverse().join("");
 }
 
 console.log(revNum(12345));
 
-num = [1,2,3,4];
+num = [1, 2, 3, 4];
 
 //Array//
 
-var test = num.reverse();
+const test = num.reverse();
 console.log(test);
--- 6_OddEven/oddeven.js
+++ 6_OddEven/oddeven.js
@@ -1,7 +1,7 @@
-var nums = [1,2,3,4,5,6,7,8,10,12,15,16];
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 10, 12, 15, 16];
 
-var oddEven = nums.forEach(num => {
-    if(num % 2 === 0){
+const oddEven = nums.forEach(num => {
+    if (num % 2 === 0) {
         console.log("Even");
     } else {
         console.log("Odd");
