92378657ae79113ac6b2ee061296e3387872adde
Rezadi Falah <rezadifalah@gmail.com>
2021-02-24 20:08:11 +0700

    added

---
+++ 5_Prime/prime.js
@@ -0,0 +1,23 @@
+function isPrime(num) {
+
+
+    if (num < 2) {
+        return false;
+    }
+    if (num === 2) {
+        return true;
+    }
+
+    let root = Math.sqrt(num);
+
+    for (i = 2; i <= root; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+
+}
+
+console.log(isPrime(12));
