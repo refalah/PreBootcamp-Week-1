81dc53ff978627a14cc0abfcd18016055efebc8b
Rezadi Falah <rezadifalah@gmail.com>
2021-02-24 13:42:27 +0700

    add

---
+++ 3_Looping/looping.js
@@ -0,0 +1,20 @@
+//Plus 1
+
+num = 15;
+for (i = 0; i <= num; i++) {
+    console.log(i);
+}
+
+//Kuadrat
+
+num = 15;
+for (i = 1; i <= num; i++) {
+    console.log(Math.pow(i,2));
+}
+
+
+//Multiply by 3
+num = 20;
+for (i = 1; i <= num; i++) {
+    console.log(i * 3);
+}
---
+++ 4_Convert/convert.js
@@ -0,0 +1,20 @@
+
+
+function convert(num){
+    if(num >= 86 && num <= 100){
+        console.log("A");
+    } else if (num >= 70 && num <= 85){
+        console.log("B");
+    } else if (num >= 50 && num <= 69){
+        console.log("C");
+    } else if (num >= 30 && num <= 49){
+        console.log("D");
+    } else if (num >= 0 && num <= 29){
+        console.log("E");
+    } else {
+        console.log("Invalid input");
+    }
+    
+}
+
+convert(100);
